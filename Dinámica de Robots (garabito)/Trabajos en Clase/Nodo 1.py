eb4eda65ec59b8e91c1e80d50bd89a1d34b4120c
Altojonathanale <46728096+Altojonathanale@users.noreply.github.com>
2020-02-17 20:00:52 -0600

    Add files via upload

---
+++ Dinámica de Robots (garabito)/Trabajos en Clase/Nodo 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#encoding: utf8
+
+def Nodo():
+    hello_str="Hola mundo"
+    print(hello_str)
+
+    if__name__=='__main__';
+        try:
+            nodo():
+                except
+                print("Hola muundo"):
+                    
+            
